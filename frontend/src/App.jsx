4f437416d3c8a48d5375b15831f7d024611fa6c1
User <unicaclubms.trafego@gmail.com>
2025-08-06 02:13:49 -0300

    remover login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+// import ProtectedRoute from './components/ProtectedRoute';
 
 // Importar páginas
 import Login from './pages/Login';
@@ -36,29 +36,13 @@ function App() {
               <Route path="/register" element={<Register />} />
               
               {/* Rotas protegidas */}
-              <Route path="/" element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              } />
+              <Route path="/" element={<Dashboard />} />
               
-              <Route path="/profile" element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              } />
+              <Route path="/profile" element={<Profile />} />
               
-              <Route path="/profile/edit" element={
-                <ProtectedRoute>
-                  <EditProfile />
-                </ProtectedRoute>
-              } />
+              <Route path="/profile/edit" element={<EditProfile />} />
               
-              <Route path="/settings" element={
-                <ProtectedRoute>
-                  <Settings />
-                </ProtectedRoute>
-              } />
+              <Route path="/settings" element={<Settings />} />
               
               {/* Rota padrão - redirecionar para dashboard */}
               <Route path="*" element={<Navigate to="/" replace />} />
